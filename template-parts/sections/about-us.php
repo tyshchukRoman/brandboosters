449537380d6d7d07ec3e9dc8cd6603cbea9d4639
Roman Tyshchuk <roman.t@codelibry.com>
2024-06-08 13:23:10 +0300

    full-height-image

--- template-parts/sections/about-us.php
+++ template-parts/sections/about-us.php
@@ -1,6 +1,6 @@
 <section class="about-us | section bg-primary-700 clr-neutral-000">
   <div class="container">
-    <div class="even-columns">
+    <div class="even-columns align-items-center">
       <div class="flow">
         <h2 class="heading-2 clr-neutral-000">About us</h2>
         <p>BrandBoosters was founded with the mission of <strong>helping businesses of all sizes</strong> achieve
@@ -9,8 +9,8 @@
             results</strong>.</p>
         <a href="" class="button button--accent">Learn more about us</a>
       </div>
-      <div>
-        <img class="round-top bg-brand-primary-200 position--right" src="<?php src('girl-on-phone.webp') ?>" alt="">
+      <div class="full-height-image position--right" style="--space-top: 2rem">
+        <img class="bg-primary-200" src="<?php src('girl-on-phone.webp') ?>" alt="">
       </div>
     </div>
   </div>
--- template-parts/sections/features-grid.php
+++ template-parts/sections/features-grid.php
@@ -2,22 +2,22 @@
   <div class="container flow">
     <h2 class="heading-2 text-center">Features</h2>
     <div class="offset-grid">
-      <div class="card bg-accent-100">
+      <div class="card | bg-accent-100">
         <h3 class="card__heading">Content Creation</h3>
         <p>Reach your ideal customers with precision through our targeted advertising campaigns.</p>
         <a href="#">Try now</a>
       </div>
-      <div class="card bg-yellow-300">
+      <div class="card | bg-yellow-300">
         <h3 class="card__heading">Targeted Advertising</h3>
         <p>Reach your ideal customers with precision through our targeted advertising campaigns.</p>
         <a href="">Try now</a>
       </div>
-      <div class="card bg-pink-300">
+      <div class="card | bg-pink-300">
         <h3 class="card__heading">Social Media</h3>
         <p>Let us handle your social media presence and engage with your audience on a personal level.</p>
         <a href="#">Try now</a>
       </div>
-      <div class="card bg-cyan-300">
+      <div class="card | bg-cyan-300">
         <h3 class="card__heading">Analytics</h3>
         <p>Stay on top of your campaign’s performance with our detailed analytics and reporting.</p>
         <a href="#">Try now</a>
